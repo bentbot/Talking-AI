2693f219605c1d82318e7c89959b8c7971feae01
Liam J. H <liamhogan7@gmail.com>
2023-04-23 23:43:08 -0400

    Format

--- Speaking_AI.php
+++ Speaking_AI.php
@@ -13,6 +13,8 @@
 	php Speaking_AI.php [chat]('Ask me a question...') [pitch 0.25-4] [speakingRate] [volume 0-1] [voice] [language]
 */
 
+$openai_api_key = 'OPEN_API_KEY';
+
 global $filters;
 $filters = array_fill(0, 3, null);
 for($i = 1; $i < $argc; $i++) {
@@ -24,18 +26,20 @@ $pitch = isset($filters[1])?$filters[1]:0.7;
 $speakingRate = isset($filters[2])?$filters[2]:0.3;
 $volume = isset($filters[3])?$filters[3]:1;
 $voice = isset($filters[4])?$filters[4]:'en-US-Studio-M';
-$language = isset($filters[5])?$filters[5]:'en-us';
+$file = isset($filters[5])?$filters[5]:'ai_speaking';
+$language = isset($filters[6])?$filters[6]:'en-us';
 if(!$prompt||$prompt==""||$prompt=="'-h'"||$prompt=="'--help'") {
 	echo("  Run the script with regular script notation. Example:\n");
 	echo("   ai --voices [see all voices available]\n");
-	echo("   ai [chat] 'make a demand' [pitch] 2 [speakingRate] 2.3 [volume] 1 [voice] 'en-US-Studio-M' [language] 'en-us'\n");
+	echo("   ai [chat] 'make a demand' [pitch] 2 [speakingRate] 2.3 [volume] 1 [voice] 'en-US-Studio-M' [MP3 file] 'file_name' [language] 'en-us'\n");
 	echo("      [chat] 'Ask for something here' \n");
 	echo("      [pitch] 0.9 / 1 / 1.3 / 2\n");
 	echo("      [speakingRate] 0.8 / 1 / 1.2 / 2.3\n");
 	echo("      [volume] 0.2 / 0.4 / 0.6 / 1\n");
 	echo("      [voice] en-US-Studio-M\n");
+	echo("      [filename] 'voice_file' [output the spoken audio to MP3]\n");
 	echo("      [language] en-us\n");
-        echo("  Examples:\n");
+	echo("  Examples:\n");
 	echo("   ai what is the sum of pi\n");
 	echo("   php Speaking AI.php --voices\n");
 	echo("   php ./Speaking\ AI.php \"where's pluto\" 1 1 1 'en-US-Neural2-H';\n");
@@ -47,92 +51,88 @@ if(!$prompt||$prompt==""||$prompt=="'-h'"||$prompt=="'--help'") {
 	$voice = available_voices(true);
 }
 
-$openai_api_key = 'OPEN_API_KEY';
-
 $url = 'https://api.openai.com/v1/chat/completions';
-
 $post_data = [
-    "model"=>"gpt-3.5-turbo",
+	"model"=>"gpt-3.5-turbo",
 		"messages"=>[[
 			"role"=> "user", 
 			"content"=> $prompt
 		]]
 ];
-
 $curl = curl_init();
 curl_setopt_array($curl, array(
-    CURLOPT_URL => $url,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_POST => true,
-    CURLOPT_POSTFIELDS => json_encode($post_data),
-    CURLOPT_HTTPHEADER => array(
-        'Content-Type: application/json',
-        'Authorization: Bearer ' . $openai_api_key,
-    ),
+	CURLOPT_URL => $url,
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_POST => true,
+	CURLOPT_POSTFIELDS => json_encode($post_data),
+	CURLOPT_HTTPHEADER => array(
+		'Content-Type: application/json',
+		'Authorization: Bearer ' . $openai_api_key,
+	),
 ));
 $response = curl_exec($curl);
 curl_close($curl);
-
 if(isset($response)){
 	$data = json_decode($response, true);
-    if(isset($data)){
-	if(isset($data['choices'][0]['message'])){
-    		$text = $data['choices'][0]['message']['content'];
-	    	echo("\n	".$text."\n\n");
-	    	read($text,$language,$voice,$pitch,$speakingRate,$volume);
-	} else {
-		print_r($data);
+	if(isset($data)){
+		if(isset($data['choices'][0]['message'])){
+				$text = $data['choices'][0]['message']['content'];
+				echo("\n	".$text."\n\n");
+				read($text,$language,$voice,$pitch,$speakingRate,$volume,$file);
+		} else {
+			print_r($data);
+		}
 	}
-    }
 }
-
-function read($text,$language,$voice,$pitch,$speakingRate,$vol) {
-	$file='ai_speaking';
+function read($text,$language,$voice,$pitch,$speakingRate,$vol,$file) {
+	
 	$google_api_key = "GOOGLE_API_KEY";
-	$filename = str_replace(' ', '_', strtolower($file)).'.mp3';
-	$myfile = fopen($filename, "w") or die("Unable to open file!");
-	$post_data = [
-		'input' => [
-			'text' => $text
-		],
-		'voice' => [
-			'languageCode' => $language,
-			'name' => $voice
-		],
-		'audioConfig' => [
-			'audioEncoding' => 'MP3',
-			'pitch' => $pitch,
-			'speakingRate' => $speakingRate,
-		]
-	];
-	$url = "https://texttospeech.googleapis.com/v1/text:synthesize";
-	$post = json_encode($post_data);
-	$google_api_ch = curl_init();
-	curl_setopt($google_api_ch, CURLOPT_URL, $url);
-	curl_setopt($google_api_ch, CURLOPT_HTTPHEADER, [
-		'X-Goog-Api-Key: '.$google_api_key,
-		'Content-Type: application/json; charset=utf-8'
-	]);
-	curl_setopt($google_api_ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($google_api_ch, CURLOPT_POST,1);
-	curl_setopt($google_api_ch, CURLOPT_POSTFIELDS, $post);
-	$result=curl_exec( $google_api_ch );
-	curl_close( $google_api_ch );
-	$content = json_decode($result);
-	if(isset($content->error)) {
-		if($content->error->message!='This request contains sentences that are too long.')
-			print_r($content->error->message);
-	} else {
-		$base64 = $content->audioContent;
-		$mp3 = base64_decode($base64);
-		fwrite($myfile, $mp3);
-		fclose($myfile);
-		chmod($filename, 0755);
-		play($filename,$vol); // To play the file
-		unlink($filename);
+
+	if($voice&&$voice!='0'&&$voice!='false'&&$file&&$file!='false'&&$file!='0') {
+		$filename = str_replace(' ', '_', strtolower($file)).'.mp3';
+		$myfile = fopen($filename, "w") or die("Unable to open file!");
+		$post_data = [
+			'input' => [
+				'text' => $text
+			],
+			'voice' => [
+				'languageCode' => $language,
+				'name' => $voice
+			],
+			'audioConfig' => [
+				'audioEncoding' => 'MP3',
+				'pitch' => $pitch,
+				'speakingRate' => $speakingRate,
+			]
+		];
+		$url = "https://texttospeech.googleapis.com/v1/text:synthesize";
+		$post = json_encode($post_data);
+		$google_api_ch = curl_init();
+		curl_setopt($google_api_ch, CURLOPT_URL, $url);
+		curl_setopt($google_api_ch, CURLOPT_HTTPHEADER, [
+			'X-Goog-Api-Key: '.$google_api_key,
+			'Content-Type: application/json; charset=utf-8'
+		]);
+		curl_setopt($google_api_ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($google_api_ch, CURLOPT_POST,1);
+		curl_setopt($google_api_ch, CURLOPT_POSTFIELDS, $post);
+		$result=curl_exec( $google_api_ch );
+		curl_close( $google_api_ch );
+		$content = json_decode($result);
+		if(isset($content->error)) {
+			if($content->error->message!='This request contains sentences that are too long.')
+				print_r($content->error->message);
+		} else if($file&&$file!='false'&&$file!='0') {
+			$base64 = $content->audioContent;
+			$mp3 = base64_decode($base64);
+			fwrite($myfile, $mp3);
+			fclose($myfile);
+			chmod($filename, 0755);
+			if($vol>0)play($filename,$vol);
+			if($file=='ai_speaking')unlink($filename);
+		}
 	}
 }
-
 function play($filename,$volumes) {
 	if(isset($volumes)) {
 		if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
@@ -149,7 +149,6 @@ function play($filename,$volumes) {
 	}
 	exec($cmd);
 }
-
 function available_voices($rand) {
 	$voices = [
 		"en-US-Neural2-A",
