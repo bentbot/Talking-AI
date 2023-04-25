a5565322a867c2b9154c4737cf020abc02af40f8
Liam J. H <liamhogan7@gmail.com>
2023-04-25 14:49:11 -0400

    Update Speaking_AI.php

--- Speaking_AI.php
+++ Speaking_AI.php
@@ -2,30 +2,38 @@
 
 /**
 * Title: Talking AI Script
-* Author: Liam Hogan <bentbot@outlook.com>
-* Date: April 23, 2023
-* Reference: https://cloud.google.com/text-to-speech https://chat.openai.com/
-* Required: afplay google_api_key openai_api_key
+* Author: L. Hogan <bentbot@outlook.com>
+* Date Created: April 18, 2023
+* Last Updated: April 25, 2023
+* References: https://chat.openai.com/ https://cloud.google.com/text-to-speech
+* Requirements: OpenAI API Key, Google TTS API Key, afplay / ffmpeg
+*
+* Installation:
+*  $ export OPEN_AI_API_KEY="your-key-here"
+*  $ export GOOGLE_TTS_KEY="your-key-here";
+*
+* Description:
+*  Run this script with regular PHP notation. Ex.
+*	$ php Speaking_AI.php [chat 'Ask me a question...'] [pitch 0.25-4] [speakingRate] [volume 0-1] [voice] [language]
+*  Or move ./ai to /usr/local/bin to be able to run with the shortcut. Ex.
+*   $ ai tell me the top ten hair bands of the seventies
 **/
 
-/* 
-	Run this script with regular PHP notation. Ex.
-	php Speaking_AI.php [chat]('Ask me a question...') [pitch 0.25-4] [speakingRate] [volume 0-1] [voice] [language]
-*/
+// $ export OPEN_AI_API_KEY="your-key-here"
+$openai_api_key = 'OPEN_API_KEY';//OPEN_API_KEY
 
-$openai_api_key = 'OPEN_API_KEY';
-
+$env_open_ai=getenv('OPEN_AI_API_KEY');
+if(isset($env_open_ai)&&$env_open_ai!='')$openai_api_key=$env_open_ai;
 global $filters;
 $filters = array_fill(0, 3, null);
 for($i = 1; $i < $argc; $i++) {
 	$filters[$i - 1] = $argv[$i];
 }
-global $volumes;
 $prompt = isset($filters[0])?$filters[0]:false;
-$pitch = isset($filters[1])?$filters[1]:0.7;
-$speakingRate = isset($filters[2])?$filters[2]:0.3;
-$volume = isset($filters[3])?$filters[3]:1;
-$voice = isset($filters[4])?$filters[4]:'en-US-Studio-M';
+$pitch = isset($filters[1])?$filters[1]:0.87;
+$speakingRate = isset($filters[2])?$filters[2]:0.9;
+$volume = isset($filters[3])?$filters[3]:0.60556;
+$voice = isset($filters[4])?$filters[4]:'en-US-Neural2-H';
 $file = isset($filters[5])?$filters[5]:'ai_speaking';
 $language = isset($filters[6])?$filters[6]:'en-us';
 if(!$prompt||$prompt==""||$prompt=="'-h'"||$prompt=="'--help'") {
@@ -85,8 +93,11 @@ if(isset($response)){
 }
 function read($text,$language,$voice,$pitch,$speakingRate,$vol,$file) {
 	
-	$google_api_key = "GOOGLE_API_KEY";
+	// $ export GOOGLE_TTS_KEY="your-key-here";
+	$google_api_key = "GOOGLE_API_KEY"; //GOOGLE_API_KEY
 
+	$env_google_tts=getenv('GOOGLE_TTS_KEY');
+	if(isset($env_google_tts)&&$env_google_tts!='')$google_api_key=$env_google_tts;
 	if($voice&&$voice!='0'&&$voice!='false'&&$file&&$file!='false'&&$file!='0') {
 		$filename = str_replace(' ', '_', strtolower($file)).'.mp3';
 		$myfile = fopen($filename, "w") or die("Unable to open file!");
@@ -120,7 +131,7 @@ function read($text,$language,$voice,$pitch,$speakingRate,$vol,$file) {
 		$content = json_decode($result);
 		if(isset($content->error)) {
 			if($content->error->message!='This request contains sentences that are too long.')
-				print_r($content->error->message);
+				echo($content->error->message);
 		} else if($file&&$file!='false'&&$file!='0') {
 			$base64 = $content->audioContent;
 			$mp3 = base64_decode($base64);
@@ -149,48 +160,12 @@ function play($filename,$volumes) {
 	exec($cmd);
 }
 function available_voices($rand) {
-	$voices = [
-		"en-US-Neural2-A",
-		"en-US-Neural2-C",
-		"en-US-Neural2-D",
-		"en-US-Neural2-E",
-		"en-US-Neural2-F",
-		"en-US-Neural2-G",
-		"en-US-Neural2-H",
-		"en-US-Neural2-I",
-		"en-US-Neural2-J",
-		"en-US-Studio-M",
-		"en-US-Studio-O",
-		"en-US-Wavenet-G",
-		"en-US-Wavenet-H",
-		"en-US-Wavenet-I",
-		"en-US-Wavenet-J",
-		"en-US-Wavenet-A",
-		"en-US-Wavenet-B",
-		"en-US-Wavenet-C",
-		"en-US-Wavenet-D",
-		"en-US-Wavenet-E",
-		"en-US-Wavenet-F",
-		"en-US-News-K",
-		"en-US-News-L",
-		"en-US-News-M",
-		"en-US-News-N",
-		"en-US-Standard-A",
-		"en-US-Standard-B",
-		"en-US-Standard-C",
-		"en-US-Standard-D",
-		"en-US-Standard-E",
-		"en-US-Standard-F",
-		"en-US-Standard-G",
-		"en-US-Standard-H",
-		"en-US-Standard-I",
-		"en-US-Standard-J",
-	];
+	$voices = ["en-US-Neural2-A","en-US-Neural2-C","en-US-Neural2-D","en-US-Neural2-E","en-US-Neural2-F","en-US-Neural2-G","en-US-Neural2-H","en-US-Neural2-I","en-US-Neural2-J","en-US-Studio-M","en-US-Studio-O","en-US-Wavenet-G","en-US-Wavenet-H","en-US-Wavenet-I","en-US-Wavenet-J","en-US-Wavenet-A","en-US-Wavenet-B","en-US-Wavenet-C","en-US-Wavenet-D","en-US-Wavenet-E","en-US-Wavenet-F","en-US-News-K","en-US-News-L","en-US-News-M","en-US-News-N","en-US-Standard-A","en-US-Standard-B","en-US-Standard-C","en-US-Standard-D","en-US-Standard-E","en-US-Standard-F","en-US-Standard-G","en-US-Standard-H","en-US-Standard-I","en-US-Standard-J"];
 	if($rand) {
 		$r=rand(0,count($voices)-1);
 		return $voices[$r];
 	} else {
-		print_r(" Available voices:\n");
-		foreach ($voices as $key => $value) print_r("  ".$value."\n");
+		echo(" Available voices:\n");
+		foreach ($voices as $key => $value) echo("  ".$value."\n");
 	}
 }
